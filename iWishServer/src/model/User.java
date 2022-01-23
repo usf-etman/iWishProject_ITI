6aca98ab9ced1039f4ece2339b849289a54db2ec
Mariem Mohamed <mariemmohamed154@gmail.com>
2022-01-23 13:03:01 +0200

    handle login page with json in both client side &server side

--- iWishServer/src/model/User.java
+++ iWishServer/src/model/User.java
@@ -2,37 +2,49 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
- package model;
+package model;
 
 public class User {
-    
+
     private String username;
     private String email;
     private String password;
     private String securityQuestion;
     //private byte[] hash;
-      public User( String email, String securityQuestion) {
-        //this.username = username;
-        this.email = email;
-       // this.password = password;
-        this.securityQuestion = securityQuestion;
-        //this.hash = hash;
-    }
 
-      public User( String username,String email, String password) {
-        this.username = username;
-       this.email = email;
-        this.password = password;
-        //this.securityQuestion = securityQuestion;
-        //this.hash = hash;
-    }
-    
     public User(String username, String email, String password, String securityQuestion) {
         this.username = username;
         this.email = email;
         this.password = password;
         this.securityQuestion = securityQuestion;
         //this.hash = hash;
+    }
+
+    public User(String email, String securityQuestion) {
+        //this.username = username;
+        this.email = email;
+        // this.password = password;
+        this.securityQuestion = securityQuestion;
+        //this.hash = hash;
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        //this.securityQuestion = securityQuestion;
+        //this.hash = hash;
+    }
+
+    public User() {
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getUsername() {
