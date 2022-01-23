1b924dcf9bfc456c45c135b6c35994734e43552f
Mariem Mohamed <mariemmohamed154@gmail.com>
2022-01-23 13:01:04 +0200

    handle login page with json in both client side &server side

--- iWishClient/src/Controller/MainClass.java
+++ iWishClient/src/Controller/MainClass.java
@@ -7,6 +7,7 @@ package Controller;
 
 import View.LoginUI;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -17,16 +18,20 @@ import javafx.stage.Stage;
  * @author Mariem
  */
 public class MainClass extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
-        LoginUI root = new LoginUI() ;
+        LoginUI root = new LoginUI();
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
         stage.show();
         LoginController lc = new LoginController(stage, root);
-        
+        stage.setOnCloseRequest(e -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
     }
 
     /**
@@ -35,5 +40,5 @@ public class MainClass extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
--- iWishClient/src/model/User.java
+++ iWishClient/src/model/User.java
@@ -2,17 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
- package model;
+package model;
 
 public class User {
-    
+
     private String username;
     private String email;
     private String password;
     private String securityQuestion;
     //private byte[] hash;
-    
-    
+
     public User(String username, String email, String password, String securityQuestion) {
         this.username = username;
         this.email = email;
@@ -20,22 +19,32 @@ public class User {
         this.securityQuestion = securityQuestion;
         //this.hash = hash;
     }
-     public User( String email, String securityQuestion) {
+
+    public User(String email, String securityQuestion) {
         //this.username = username;
         this.email = email;
-       // this.password = password;
+        // this.password = password;
         this.securityQuestion = securityQuestion;
         //this.hash = hash;
     }
 
-      public User( String username,String email, String password) {
+    public User(String username, String email, String password) {
         this.username = username;
-       this.email = email;
+        this.email = email;
         this.password = password;
         //this.securityQuestion = securityQuestion;
         //this.hash = hash;
     }
+    public User() {
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getUsername() {
         return username;
